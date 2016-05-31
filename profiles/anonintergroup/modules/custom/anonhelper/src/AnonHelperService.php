35f7a7f0c23a1a4e681638ce275253727569f447
Anonymous Drupal <anondrupal@gmail.com>
2016-05-31 15:57:11 -0400

    Create initial anonorgtheme and baltimoreaatheme

---
+++ profiles/anonintergroup/modules/custom/anonhelper/src/AnonHelperService.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\anonhelper\AnonHelperService.
+ */
+
+namespace Drupal\anonhelper;
+
+use Drupal\Core\Extension\ThemeInstaller;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Entity\EntityManager;
+
+/**
+ * Class AnonHelperService.
+ *
+ * @package Drupal\anonhelper
+ */
+class AnonHelperService implements AnonHelperServiceInterface {
+
+  /**
+   * @var Drupal\Core\Extension\ThemeInstaller
+   */
+  protected $theme_installer;
+
+  /**
+   * @var Drupal\Core\Config\ConfigFactory
+   */
+  protected $config_factory;
+
+  /**
+   * @var Drupal\Core\Entity\EntityManager
+   */
+  protected $entity_manager;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(ConfigFactory $config_factory, EntityManager $entity_manager, ThemeInstaller $theme_installer) {
+    $this->config_factory = $config_factory;
+    $this->entity_manager = $entity_manager;
+    $this->theme_installer = $theme_installer;
+  }
+
+  /**
+   * Set the default theme.
+   *
+   * @param $default_theme
+   */
+  function changeTheme($default_theme) {
+    $this->theme_installer->install([$default_theme], FALSE);
+    $this->config_factory->getEditable('system.theme')
+      ->set('default', $default_theme)
+      ->save(TRUE);
+
+    // Remove navigation header blocks.
+    // @todo: why do we need to do this? Shouldn't the default blocks be
+    // picked up from anontheme.
+    $blocks = $this->entity_manager->getStorage('block')->loadMultiple($block_ids);
+    foreach ($blocks as $block) {
+      /** @var $block \Drupal\block\BlockInterface */
+      if ($block->getTheme() == $default_theme && $block->getRegion() == 'navigation') {
+        $block->setStatus(FALSE);
+        $block->save();
+      }
+    }
+  }
+
+}
---
+++ profiles/anonintergroup/modules/custom/anonhelper/src/AnonHelperServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\anonhelper\AnonHelperServiceInterface.
+ */
+
+namespace Drupal\anonhelper;
+
+/**
+ * Interface AnonHelperServiceInterface.
+ *
+ * @package Drupal\anonhelper
+ */
+interface AnonHelperServiceInterface {
+  
+  public function changeTheme($default_theme);
+}
