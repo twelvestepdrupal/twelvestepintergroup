73838823a64ac1b4120f427950523e8c2b146db5
Anonymous Drupal <anondrupal@gmail.com>
2016-06-27 19:06:31 -0400

    Issue-30: add map resize/zoom to filter locations

--- profiles/twelvestepintergroup/themes/twelvesteptheme/js/geolocation-common-map.js
+++ profiles/twelvestepintergroup/themes/twelvesteptheme/js/geolocation-common-map.js
@@ -6,7 +6,9 @@
 (function ($) {
     "use strict";
 
-    Drupal.behaviors.twelvestepLocationMap = {
+    Drupal.behaviors.twelvestepGoogleMap = {
+        obj: null,
+
         attach: function (context, settings) {
             var $map = $('#' + drupalSettings.geolocation.commonMap.id, context);
             var $container = $map.children('.geolocation-common-map-container');
@@ -21,22 +23,22 @@
                 var center = new google.maps.LatLng(0, 0);
             }
 
-            var googleMap = new google.maps.Map($container[0], {
+            Drupal.behaviors.twelvestepGoogleMap.obj = new google.maps.Map($container[0], {
                 center: center,
                 zoom: 12
             });
 
-            $map.find('.geolocation-common-map-locations .geolocation').each(function(index, item) {
-                item = $(item);
-                var position = new google.maps.LatLng(item.data('lat'), item.data('lng'));
+            var $locations = $map.find('.geolocation-common-map-locations .geolocation');
+            $locations.each(function(index, item) {
+                item.position = new google.maps.LatLng($(item).data('lat'), $(item).data('lng'));
 
-                bounds.extend(position);
+                bounds.extend(item.position);
 
                 var marker = new google.maps.Marker({
-                    position: position,
-                    map: googleMap,
-                    title: item.children('h2').text(),
-                    content: item.html()
+                    position: item.position,
+                    map: Drupal.behaviors.twelvestepGoogleMap.obj,
+                    title: $(item).children('h2').text(),
+                    content: $(item).html()
                 });
 
                 marker.addListener('click', function() {
@@ -49,30 +51,49 @@
                     var bubble = new google.maps.InfoWindow({
                         content: marker.content
                     });
-                    bubble.open(googleMap, marker);
+                    bubble.open(Drupal.behaviors.twelvestepGoogleMap.obj, marker);
                     bubbles.push(bubble)
                 });
             });
 
-            // Always show as much of the map as possible.
+            // Show as much of the map as possible.
             $(window).resize(function() {
-                Drupal.behaviors.twelvestepLocationMap.resizeMap($container, googleMap, bounds);
+                Drupal.behaviors.twelvestepGoogleMap.resizeMap($container, Drupal.behaviors.twelvestepGoogleMap.obj, bounds);
             });
             // @todo: what are we waiting on here? something with the map,
             // without this delay, the initial map is too high (on Chrome).
             setTimeout(function() {
-                Drupal.behaviors.twelvestepLocationMap.resizeMap($container, googleMap, bounds);
-            }, 100)
+                Drupal.behaviors.twelvestepGoogleMap.resizeMap($container, bounds);
+            }, 100);
+
+            // When the boundaries change, only show locations in bounds.
+            google.maps.event.addListener(Drupal.behaviors.twelvestepGoogleMap.obj, 'zoom_changed', Drupal.behaviors.twelvestepGoogleMap.zoomMap);
+            google.maps.event.addListener(Drupal.behaviors.twelvestepGoogleMap.obj, 'dragend', Drupal.behaviors.twelvestepGoogleMap.zoomMap);
         },
 
-        resizeMap: function($container, googleMap, bounds) {
+        resizeMap: function($container, bounds) {
             // Resize the map to take the entire viewport height (less 10px).
             var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
             var top = $container.offset().top;
             $container.css('height', vh - top - 10);
             // Resize the map to fit better.
-            googleMap.fitBounds(bounds);
-            $(googleMap.getDiv()).toggle().toggle();
+            Drupal.behaviors.twelvestepGoogleMap.obj.fitBounds(bounds);
+            var $div = Drupal.behaviors.twelvestepGoogleMap.obj.getDiv();
+            $($div).toggle().toggle();
+        },
+
+        zoomMap: function() {
+            var boundary = Drupal.behaviors.twelvestepGoogleMap.obj.getBounds();
+            var $map = $('#' + drupalSettings.geolocation.commonMap.id);
+            var $locations = $map.find(' .geolocation-common-map-locations .geolocation');
+            $locations.each(function(index, item) {
+                if (boundary.contains(item.position)) {
+                    $(item).show();
+                }
+                else {
+                    $(item).hide();
+                }
+            });
         }
     };
 
